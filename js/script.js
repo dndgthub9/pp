3800df483c8106f1b5395f1e2b42f1cea600386e
dndgthub9 <dwiadinda111@gmail.com>
2025-01-30 09:16:45 +0700

    first github

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =============== typing animation ============== */
+var typed = new Typed(".typing",{
+    strings:["UI/UX Designer", "Front-End", "Data Analyst", "Web Designer", "Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
